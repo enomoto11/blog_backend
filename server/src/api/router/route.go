b26117c94b63fcf893413217fa88405c6a56263a
enomoto11 <t.enomoto@catallaxy>
2023-03-21 00:45:49 +0900

    コントローラ・サービス・レポジトリをセットアップする関数を用意

---
+++ server/src/api/router/route.go
@@ -0,0 +1,50 @@
+package router
+
+import (
+	"blog/api/controller"
+	"blog/api/repository"
+	"blog/api/service"
+	"blog/ent"
+	"blog/ent/migrate"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitControllers() (*gin.Engine, *ent.Client) {
+	router := gin.Default()
+
+	path := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+	entClient, err := ent.Open("mysql", path)
+	if err != nil {
+		log.Fatalf("failed connect to mysql: %v", err)
+	}
+
+	ctx := context.Background()
+	if err := entClient.Schema.Create(
+		ctx,
+		migrate.WithDropIndex(true),
+		migrate.WithDropColumn(true),
+	); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	controller := setUpController(entClient)
+
+	controller.CreateUserController(router)
+
+	return router, entClient
+}
+
+func setUpController(entClient *ent.Client) controller.UserController {
+	createUserRepo := repository.NewUserRepository(entClient)
+
+	createUserService := service.NewUserService(createUserRepo)
+
+	createUserController := controller.NewUserController(createUserService)
+
+	return createUserController
+}
