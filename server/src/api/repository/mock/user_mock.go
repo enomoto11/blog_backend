9caee47132f8d9a7c6a98dd8c54239391635b1a1
enomoto11 <t.enomoto@catallaxy>
2023-03-25 20:10:53 +0900

    mockを再生成

--- server/src/api/repository/mock/user_mock.go
+++ server/src/api/repository/mock/user_mock.go
@@ -51,11 +51,11 @@ func (mr *MockUserRepositoryMockRecorder) Create(ctx, m interface{}) *gomock.Cal
 }
 
 // FindAll mocks base method.
-func (m *MockUserRepository) FindAll(ctx context.Context) ([]*model.GETUserModel, []error) {
+func (m *MockUserRepository) FindAll(ctx context.Context) ([]*model.GETUserModel, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "FindAll", ctx)
 	ret0, _ := ret[0].([]*model.GETUserModel)
-	ret1, _ := ret[1].([]error)
+	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
