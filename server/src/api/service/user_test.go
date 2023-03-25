6a7c456f595aa84d81ef3114be9e807463fb6369
enomoto11 <t.enomoto@catallaxy>
2023-03-25 20:47:39 +0900

    サービスのテストを実装

--- server/src/api/service/user_test.go
+++ server/src/api/service/user_test.go
@@ -4,6 +4,7 @@ import (
 	"blog/api/controller/request"
 	"blog/api/model"
 	mock_repository "blog/api/repository/mock"
+	"blog/ent"
 	"context"
 	"testing"
 
@@ -81,6 +82,89 @@ func Test_UserService_CreateUser(t *testing.T) {
 
 			// Assert
 			tt.matcher(t, expected, got, err)
+		})
+	}
+}
+
+func Test_UserService_FindAllUsers(t *testing.T) {
+	ctx := context.Background()
+
+	id1 := uuid.New()
+	id2 := uuid.New()
+
+	m1, err1 := model.NewGETUser(
+		model.NewGETUserID(id1),
+		model.NewGETUserFirstName("アキ"),
+		model.NewGETUserLastName("早川"),
+		model.NewGETUserEmail("a.hayakawa@koan.com"),
+	)
+	require.NoError(t, err1)
+
+	m2, err2 := model.NewGETUser(
+		model.NewGETUserID(id2),
+		model.NewGETUserFirstName("デンジ"),
+		model.NewGETUserLastName("チェーンソーマン"),
+		model.NewGETUserEmail("d.chainsaw@koan.com"),
+	)
+	require.NoError(t, err2)
+
+	tests := []struct {
+		name          string
+		prepareFn     func(*testing.T, *ent.Client)
+		prepareMockFn func(*testing.T, *userServiceMocks)
+		matcher       func(*testing.T, []*model.GETUserModel, error)
+	}{
+		{
+			name: "正常系",
+			prepareFn: func(t *testing.T, client *ent.Client) {
+				_, err1 := client.User.Create().
+					SetID(id1).
+					SetFirstName("アキ").
+					SetLastName("早川").
+					SetEmail("a.hayakawa@koan.com").
+					SetPassword("kon_mirai4").
+					Save(ctx)
+				require.NoError(t, err1)
+
+				_, err2 := client.User.Create().
+					SetID(id2).
+					SetFirstName("デンジ").
+					SetLastName("チェーンソーマン").
+					SetEmail("d.chainsaw@koan.com").
+					SetPassword("makima_san").
+					Save(ctx)
+				require.NoError(t, err2)
+			},
+			prepareMockFn: func(t *testing.T, mocks *userServiceMocks) {
+
+				mocks.createUserRepo.EXPECT().FindAll(ctx).Return([]*model.GETUserModel{
+					m1,
+					m2,
+				}, nil)
+			},
+			matcher: func(t *testing.T, got []*model.GETUserModel, err error) {
+				assert.Nil(t, err)
+				assert.NotEmpty(t, got)
+				assert.Equal(t, []*model.GETUserModel{
+					m1,
+					m2,
+				}, got)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arange
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			svc, mocks := getServiceMocks(ctrl)
+			tt.prepareMockFn(t, mocks)
+
+			// Action
+			got, err := svc.FindAllUsers(ctx)
+
+			// Assert
+			tt.matcher(t, got, err)
 		})
 	}
 }
