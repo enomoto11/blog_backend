f06bb7fc89b838ee33d92a66a11092009bb53e58
enomoto11 <t.enomoto@catallaxy>
2023-03-25 21:56:54 +0900

    repositoryを作成

---
+++ server/src/api/repository/category.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"blog/api/model"
+	"blog/ent"
+	"context"
+)
+
+type CategoryRepository interface {
+	Create(ctx context.Context, m *model.POSTCategoryModel) (*model.POSTCategoryModel, error)
+}
+
+type categoryRepository struct {
+	client *ent.Client
+}
+
+func NewCategoryRepository(client *ent.Client) CategoryRepository {
+	return &categoryRepository{client}
+}
+
+func (r *categoryRepository) Create(ctx context.Context, m *model.POSTCategoryModel) (*model.POSTCategoryModel, error) {
+	entity, err := r.client.Category.Create().
+		SetID(m.GetID()).
+		SetName(m.GetName()).
+		Save(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return categoryModelFromEntity(entity)
+}
+
+func categoryModelFromEntity(entity *ent.Category) (*model.POSTCategoryModel, error) {
+	opts := []model.NewPOSTCategoryOption{
+		model.NewPOSTCategoryID(entity.ID),
+		model.NewPOSTCategoryName(entity.Name),
+	}
+
+	return model.NewPOSTCategory(opts...)
+}
