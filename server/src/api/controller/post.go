4a31db734eca46609775d012e1c471ccfc42268f
enomoto11 <t.enomoto@catallaxy>
2023-03-29 23:15:25 +0900

    createPost関数として切りだした

--- server/src/api/controller/post.go
+++ server/src/api/controller/post.go
@@ -24,28 +24,30 @@ func NewPostController(postService service.PostService) PostController {
 }
 
 func (c *postController) RegisterHandlers(r gin.IRouter) {
-	r.POST("post/new", func(ctx *gin.Context) {
-		var requestBody request.POSTPostRequestBody
-		if err := ctx.ShouldBindJSON(&requestBody); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	r.POST("post/new", c.createPost)
+}
 
-		validate := validator.New()
-		if err := validate.Struct(requestBody); err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+func (c *postController) createPost(ctx *gin.Context) {
+	var requestBody request.POSTPostRequestBody
+	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-		result, err := c.postService.CreatePost(ctx, requestBody)
-		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+	validate := validator.New()
+	if err := validate.Struct(requestBody); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-		ctx.JSON(http.StatusCreated, gin.H{
-			"message": "success in creating new post",
-			"result":  result,
-		})
+	result, err := c.postService.CreatePost(ctx, requestBody)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "success in creating new post",
+		"result":  result,
 	})
 }
